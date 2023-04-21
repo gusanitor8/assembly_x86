0f27a5fc84098f9c0fb8482b112068323f24bec7
Ubuntu <ubuntu@ip-172-31-81-182.ec2.internal>
2023-04-21 04:49:42 +0000

    Revert "commit inicial"
    This reverts commit 4457251420f589bf07f52d7df4bd56d53dc4fd9b.

--- programs/ex.asm
+++
@@ -1,9 +0,0 @@
-global _start
-_start:
-    mov eax,4       ;4 es nuestro dividendo
-    mov ebx,2       ;2 es nuestro divisor
-    div ebx         ;el resultado se almacena en eax
-
-    mov ebx, eax    ;movemos el resultado en eax a ebx para desplegarlo como codigo de salida
-    mov eax, 1      ;1 es corresponde al system call de salida
-    int 0x80        ;0x80 corresponde al interrupt de system call
